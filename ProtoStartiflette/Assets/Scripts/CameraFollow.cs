c544d26c58754ee6c01d3ff0904b8a47f33a876f
Axel-Girard <axel942@gmail.com>
2018-01-07 23:38:13 +0100

    proto

---
+++ ProtoStartiflette/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+
+	public float smoothSpeed = 10f;
+
+	public Vector3 offset;
+
+	/*void Update () {
+		Vector3 desiredPos = target.position + offset;
+		Vector3 smoothedPos = Vector3.Lerp (transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+		if (desiredPos.x >= 100f) {
+			desiredPos.x = 100f;
+		}
+		if (desiredPos.x <= -100f) {
+			desiredPos.x = -100f;
+		}
+		if (desiredPos.y >= 100f) {
+			desiredPos.y = 100f;
+		}
+		if (desiredPos.y <= -100f) {
+			desiredPos.y = -100f;
+		}
+		transform.position = desiredPos;
+		Debug.Log ("target pos: " + target.position);
+		Debug.Log ("camera pos: " + transform.position);
+	}*/
+}
